86dbffd0c790117b2fbc00c52d57840e2a63bfd6
Abdulwasim-S <abdulwasimsmech@gmail.com>
2023-08-24 14:02:24 +0530

    update

--- src/Pages/JobPage/JobPage.js
+++ src/Pages/JobPage/JobPage.js
@@ -4,6 +4,8 @@ import { NavLink } from 'react-router-dom';
 const JobPage = () => {
   return (
     <div className='mt-3 mb-3'>
+        <input type='text' className=''/>
+        <NavLink className='btn-post-job mt-3 mb-3' to={'/post-new-job'}>Post New Job</NavLink>
         <NavLink className='btn-post-job mt-3 mb-3' to={'/post-new-job'}>Post New Job</NavLink>
     </div>
   )
