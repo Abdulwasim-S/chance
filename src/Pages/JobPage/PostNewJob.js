9a36ee942633e21c33b83acbc2a3acae0c43b6b4
Abdulwasim-S <abdulwasimsmech@gmail.com>
2023-08-24 20:14:09 +0530

    update

--- src/Pages/JobPage/PostNewJob.js
+++ src/Pages/JobPage/PostNewJob.js
@@ -4,20 +4,6 @@ import { useNavigate } from 'react-router-dom';
 import * as yup from 'yup';
 
 const PostNewJob = () => {
-    // useEffect(async ()=>{
-    //     const response = await fetch(
-    //         "https://chance-backend.vercel.app/jobs",
-    //         {
-    //           method: "POST",
-    //           body: JSON.stringify(logInInfo),
-    //           headers: {
-    //             "Content-Type": "application/json",
-    //           },
-    //         }
-    //       );
-    //       const data = await response.json();
-    //       console.log(data)
-    // },[])
     const navTo = useNavigate();
     const [state,setState]=useState("");
   const fieldValidationSchema = yup.object({
